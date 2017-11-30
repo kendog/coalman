dfc9625c8445d3f0b6cdfb5cf8d46b72d6928977
kendog <kendog@Kennys-MacBook-Pro.local>
2017-11-29 20:53:23 -0800

    hotfix package add

--- app.py
+++ app.py
@@ -637,21 +637,33 @@ def admin_packages():
 def admin_packages_add():
     if 'submit-add' in request.form:
         # Update Metadata
-        package = Package(uuid=str(uuid.uuid4()), user_name=request.form.get('user_name'), user_email=request.form.get('user_email'))
+        user_name = request.form.get('user_name')
+        user_email = request.form.get("user_email")
+        file_ids = request.form.get("files")
+
+        package = Package(uuid=str(uuid.uuid4()), user_name=user_name, user_email=user_email)
         db.session.add(package)
         db.session.commit()
+
         package.package_status_id = 1
         package.notification_status_id = 1
         package.name = package.uuid + ".zip"
         package.path = app.config['TEMP_FOLDER']
+        package.link = app.config['DOWNLOAD_PROTOCOL'] + '://' + app.config['DOWNLOAD_DOMAIN'] + url_for(
+            'download_package', uuid=package.uuid)
         db.session.commit()
+
         # Add Files
-        file_ids = request.form.getlist('files')
         for item in file_ids:
             exists = db.session.query(File.id).filter_by(id=item).scalar()
             if exists:
                 package.files.append(File.query.filter_by(id=item).first())
                 db.session.commit()
+
+        # Send Email
+        if request.form.get("notify"):
+            send_notification(package.uuid)
+
         return redirect(url_for('admin_packages'))
     files = File.query.all()
     return render_template('admin_packages_add.html', files=files)
