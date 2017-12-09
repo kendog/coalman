bb88238761e45ff4698a1109d0bdda614fd09f2b
kendog <kendog@Kennys-MacBook-Pro.local>
2017-12-08 20:52:02 -0800

    ssl

--- server/ssl.sh
+++ server/ssl.sh
@@ -12,3 +12,7 @@ SSLCertificateKeyFile /etc/pki/tls/private/localhost.key
 #SSLCertificateChainFile /etc/pki/tls/certs/server-chain.crt
 
 grep /etc/httpd/conf.d/ssl.conf.off -vEe ^# -vEe ^[[:space:]]*# -vEe ^[[:space:]]*$
+
+
+
+sudo cp /var/www/coalman.io/server/etc/httpd/conf/ssl.conf /etc/httpd/conf/ssl.conf
