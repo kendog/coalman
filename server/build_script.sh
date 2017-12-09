ffbd92f38ff1ca87102e737bd7c06d23a5e62660
kendog <kendog@Kennys-MacBook-Pro.local>
2017-12-08 23:47:25 -0800

    ssl reorg

--- server/build_script.sh
+++ server/build_script.sh
@@ -22,30 +22,6 @@ sudo chown -R apache:apache /var/www/coalman.io
 sudo chown -R apache:apache /var/www/demo.coalman.io
 sudo chmod -R 755 /var/www/
 
-# DB
-# install mysql
-sudo yum -y install mariadb-server
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-#sudo mysql_secure_installation
-mysqladmin -u root password "$DATABASE_PASS"
-mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_ROOT_PASS') WHERE User='root'"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
-# create user
-mysql -u root -p"$DATABASE_PASS" -e "CREATE USER 'coalman'@'localhost' IDENTIFIED BY '$DATABASE_USER_PASS'"
-mysql -u root -p"$DATABASE_PASS" -e "CREATE USER 'coalman'@'%' IDENTIFIED BY '$DATABASE_USER_PASS'"
-mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL ON *.* TO 'coalman'@'localhost'"
-mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL ON *.* TO 'coalman'@'%'"
-# create db
-mysql -u root -p"$DATABASE_PASS" -e "DROP DATABASE IF EXISTS coalman"
-mysql -u root -p"$DATABASE_PASS" -e "CREATE DATABASE coalman"
-# allow httpd
-sudo setsebool -P httpd_can_network_connect_db on
-
-
 # Requirements
 sudo yum -y install git
 sudo yum -y install mod_wsgi
@@ -54,11 +30,8 @@ sudo yum -y install mod_wsgi
 sudo git clone https://github.com/kendog/coalman.git /var/www/coalman.io
 sudo chown -R apache:apache /var/www/coalman.io/uploads/
 sudo cp /var/www/coalman.io/server/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
-sudo cp /var/www/coalman.io/server/etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf
 sudo cp /var/www/coalman.io/server/etc/httpd/sites-available/coalman.io.conf /etc/httpd/sites-available/coalman.io.conf
 sudo cp /var/www/coalman.io/server/etc/httpd/sites-available/demo.coalman.io.conf /etc/httpd/sites-available/demo.coalman.io.conf
-
-#sudo cp /var/www/coalman.io/server/var/www/demo.coalman.io/index.html /var/www/demo.coalman.io/index.html
 
 sudo ln -s /etc/httpd/sites-available/coalman.io.conf /etc/httpd/sites-enabled/coalman.io.conf
 sudo ln -s /etc/httpd/sites-available/demo.coalman.io.conf /etc/httpd/sites-enabled/demo.coalman.io.conf
