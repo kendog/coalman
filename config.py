f4b1f7996f529c7f384f317d136cbcad944308f6
Kenny Chen <kennychen@Kennys-MacBook-Pro.local>
2019-10-11 23:21:52 -0700

    env vars

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from os import environ
 
 class Config:
     SESSION_TYPE = 'filesystem'
-    SQLALCHEMY_DATABASE_URI = 'db_protocol://user:pass@host:port/database'
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'secret_key'
-    JWT_SECRET_KEY = 'jwt_secret_key'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
     SECURITY_REGISTERABLE = True
-    SECURITY_PASSWORD_HASH = 'bcrypt'
-    SECURITY_PASSWORD_SALT = 'password_salt'
+    SECURITY_PASSWORD_HASH = os.environ['SECURITY_PASSWORD_HASH']
+    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
     SECURITY_USER_IDENTITY_ATTRIBUTES = ['email']
 
     DEBUG = True
@@ -23,15 +23,15 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'username'
-    MAIL_PASSWORD = 'password'
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
 
-    SITE_ADMIN_EMAIL = 'admin@beasel.io'
-    SITE_ADMIN_PASSWORD = 'admin1234'
+    SITE_ADMIN_EMAIL = os.environ['SITE_ADMIN_EMAIL']
+    SITE_ADMIN_PASSWORD = os.environ['SITE_ADMIN_PASSWORD']
 
     UPLOAD_TO_S3 = True
-    AWS_ACCESS_KEY = 'AWS_ACCESS_KEY'
-    AWS_SECRET_KEY = 'AWS_SECRET_KEY'
+    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+    AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
     S3_BUCKET = 'S3_BUCKET'
     S3_URL = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
--- run_app.sh
+++ run_app.sh
@@ -2,4 +2,5 @@
 export FLASK_APP=wsgi.py
 export FLASK_DEBUG=1
 export APP_CONFIG_FILE=config.py
+
 flask run
